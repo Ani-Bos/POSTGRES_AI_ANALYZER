ff10edca657cf734a4f534dac12de9feb6b1fe89
Aniket Bose <97694136+Ani-Bos@users.noreply.github.com>
2026-02-08 17:52:17 +0530

    fixing gitignore

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <h1 className='flex justify-center bg-amber-300 text-green-500'>Building an AI POSTGRES QUERY ANALYZER</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
